5ab5b6181f36f7b6209d3179cd67fbbbe6d0590f
João Gabriel <joaodesignerg@gmail.com>
2026-01-29 10:11:26 -0300

    menu bar add x

--- js/menubar.js
+++ js/menubar.js
@@ -2,12 +2,12 @@ $(function () {
 
   $('.menu-toggle').click(function () {
     $('nav.mobile').slideToggle(250);
+    $(this).find('i').toggleClass('fa-bars fa-xmark');
   });
 
   // fecha menu ao clicar em um link
   $('nav.mobile a').click(function () {
-    $('nav.mobile').slideToggle(250);
-    $(this).find('i').toggleClass('fa-bars fa-xmark');
+    $('nav.mobile').slideUp(250);
   });
 
 });
